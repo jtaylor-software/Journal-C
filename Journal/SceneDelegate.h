21152ae997e168a2d3af277b95c4cbf97be5a220
Jeremy Taylor <jtaylor@jctaylor.org>
2020-11-30 14:05:30 -0600

    Initial Commit

---
+++ Journal/SceneDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SceneDelegate.h
+//  Journal
+//
+//  Created by Jeremy Taylor on 11/30/20.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
---
+++ Journal/ViewController.m
@@ -0,0 +1,22 @@
+//
+//  ViewController.m
+//  Journal
+//
+//  Created by Jeremy Taylor on 11/30/20.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
